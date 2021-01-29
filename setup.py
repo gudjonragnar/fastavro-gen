98f1ba2de389d54a5c753c484e8df74ebe1bba3e
Guðjón <gudjon.8@gmail.com>
2021-01-29 20:24:03 +0000

    Basic setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+version = "0.0.1"
+
+
+setup(
+    name="fastavro-gen",
+    version=version,
+    description="Generation of classes from Avro schemas",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Guðjón Ragnar Brynjarsson",
+    license="MIT",
+    url="https://github.com/gudjonragnar/fastavro-gen",
+    packages=["fastavro_gen"],
+    entry_points={
+        "console_scripts": [
+            "fastavro_gen = fastavro_gen.__main__:main",
+        ]
+    },
+    classifiers=[],
+    python_requires=">=3.8",
+    extras_require={
+        "codecs": ["python-snappy", "zstandard", "lz4"],
+        "snappy": ["python-snappy"],
+        "zstandard": ["zstandard"],
+        "lz4": ["lz4"],
+    },
+    requires=["fastavro"],
+    tests_require=["pytest", "mypy"],
+    package_data={"fastavro_gen": ["py.typed"]},
+)
